a03be4c14572ffe792bdcd5d781e7c178713a978
Driss <drissaitkassi@gmail.com>
2023-06-01 21:33:36 +0100

    add radio button selection for each operation along with it form showing on click

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-account',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+  selectedForm !: string;
+  versementForm! : FormGroup;
+  retraitForm !: FormGroup;
+  virementForm ! : FormGroup;
 
-  constructor() { }
+  constructor(private fb :FormBuilder) { }
 
   ngOnInit(): void {
+    this.versementForm=this.fb.group({
+      "mntVersement":this.fb.control(''),
+      "numCompte":this.fb.control(''),
+
+    })
+
+
+    this.retraitForm=this.fb.group({
+      "mntRetrait":this.fb.control(''),
+      "numCompte":this.fb.control(''),
+
+    })
+
+    this.virementForm=this.fb.group({
+      "mntVirement":this.fb.control(''),
+      "numCompteDebit":this.fb.control(''),
+      "numCompteCredit":this.fb.control(''),
+
+    })
+
   }
 
 }
